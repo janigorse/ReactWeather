fae336df25e973897bb09fc9d0a4f79d44e95080
jani gorse <jani.gorse@gmail.com>
2017-02-22 23:07:59 +0100

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,11 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-            <h3>Examples component!</h3>
+            <div>
+                <h3>Examples component!</h3>
+                <p>Welcome to examples page!</p>
+            </div>
+            
         )
 };
 
