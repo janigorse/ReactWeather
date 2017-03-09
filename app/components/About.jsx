019c34b60cee905a7f4205742643f0027d7f9ea9
jani gorse <jani.gorse@gmail.com>
2017-03-09 21:26:41 +0100

    Adding some custom styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
     return (
             <div>
-                <h1 className="text-center">About</h1>
+                <h1 className="text-center page-title">About</h1>
                 <p>Welcome to about page!</p>   
                 <a href="https://twitter.com/janigorse" >twitter</a>
             </div>
